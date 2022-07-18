c86b98eb9dcc57f2fd7e97ef7eb4f1995878a3de
chrisphine <onyangoketta@gmail.com>
2022-07-18 14:26:16 +0300

    import useeffect from react

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
